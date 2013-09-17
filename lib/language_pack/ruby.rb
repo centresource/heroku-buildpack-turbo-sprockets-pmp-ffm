5c13111f6a2478f589543838e10085bec98d99f8
Jeremy Holland <jeremy@jeremypholland.com>
2013-09-17 14:01:38 -0500

    debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -467,9 +467,9 @@ ERROR
         bundler_path         = "#{pwd}/#{slug_vendor_base}/gems/#{BUNDLER_GEM_PATH}/lib"
         # we need to set BUNDLE_CONFIG and BUNDLE_GEMFILE for
         # codon since it uses bundler.
-        puts "CPATH: #{$CPATH}"
-        puts "CPPATH: #{$CPPATH}"
-        puts "LIBRARY_PATH: #{$LIBRARY_PATH}"
+        puts "CPATH: #{ENV['CPATH']}"
+        puts "CPPATH: #{ENV['CPPATH']}"
+        puts "LIBRARY_PATH: #{ENV['LIBRARY_PATH']}"
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:#{libxmlsec1_include}:#{libxml_include}:$CPATH CPPATH=#{yaml_include}:#{libxmlsec1_include}:#{libxml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:#{libxmlsec1_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         env_vars      += " BUNDLER_LIB_PATH=#{bundler_path}" if ruby_version == "ruby-1.8.7"
         puts "ENV VARS: #{env_vars}"
