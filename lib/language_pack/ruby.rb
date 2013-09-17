96391c6dc9fd3983477b949ac7bf38bceab94bd0
Jeremy Holland <jeremy@jeremypholland.com>
2013-09-17 11:15:40 -0500

    debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -469,6 +469,7 @@ ERROR
         env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:$CPATH CPPATH=#{yaml_include}:#{libxmlsec1_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:#{libxmlsec1_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         env_vars      += " BUNDLER_LIB_PATH=#{bundler_path}" if ruby_version == "ruby-1.8.7"
         puts "Running: #{bundle_command}"
+        puts "ENV VARS: #{env_vars}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
 
       end
