036532ed3d5f4b7ada13fe460302f2c6762d73f8
Jeremy Holland <jeremy@jeremypholland.com>
2013-09-17 15:18:21 -0500

    debugging

--- lib/language_pack/ruby.rb
+++ lib/language_pack/ruby.rb
@@ -463,6 +463,7 @@ ERROR
         libxmlsec1_include   = File.expand_path("#{libxmlsec1_dir}/include/xmlsec1")
         libxml_include       = "/usr/include/libxml2"
         libxmlsec1_lib       = File.expand_path("#{libxmlsec1_dir}/lib")
+        post_libxmlsec1_lib  = "/app/#{libxmlsec1_dir}/lib"
         pwd                  = run("pwd").chomp
         bundler_path         = "#{pwd}/#{slug_vendor_base}/gems/#{BUNDLER_GEM_PATH}/lib"
         # we need to set BUNDLE_CONFIG and BUNDLE_GEMFILE for
@@ -470,7 +471,7 @@ ERROR
         puts "CPATH: #{ENV['CPATH']}"
         puts "CPPATH: #{ENV['CPPATH']}"
         puts "LIBRARY_PATH: #{ENV['LIBRARY_PATH']}"
-        env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:#{libxmlsec1_include}:#{libxml_include}:$CPATH CPPATH=#{yaml_include}:#{libxmlsec1_include}:#{libxml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:#{libxmlsec1_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
+        env_vars       = "env BUNDLE_GEMFILE=#{pwd}/Gemfile BUNDLE_CONFIG=#{pwd}/.bundle/config CPATH=#{yaml_include}:#{libxmlsec1_include}:#{libxml_include}:$CPATH CPPATH=#{yaml_include}:#{libxmlsec1_include}:#{libxml_include}:$CPPATH LIBRARY_PATH=#{yaml_lib}:#{libxmlsec1_lib}:#{post_libxmlsec1_lib}:$LIBRARY_PATH RUBYOPT=\"#{syck_hack}\""
         env_vars      += " BUNDLER_LIB_PATH=#{bundler_path}" if ruby_version == "ruby-1.8.7"
         puts "ENV VARS: #{env_vars}"
         bundler_output << pipe("#{env_vars} #{bundle_command} --no-clean 2>&1")
